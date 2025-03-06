9a4084fca0f04597d3a2135597a840708a1a446d
ennisjo <ennisjo1@msu.edu>
2025-03-06 23:09:24 +0000

    Simple Changes (Alpine.JS)

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Add JavaScript below
+function orange(){
+    alert("hello");
+}
+
+window.onload =function(){
+const purple = document.getElementById("purple");
+
+purple.addEventListener("click", (event) => {
+    orange();
+});}
